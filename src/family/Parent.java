de4b5c809bc22cea2b2abfbd40087326e24853ce
srila <srilakshmiannamdasu@gmail.com>
2024-06-27 15:02:17 +0530

    family1

---
+++ src/family/Parent.java
@@ -0,0 +1,8 @@
+package family;
+
+public class Parent {
+    public String flatNo;
+    public String streetName;
+    public String city;
+    public String country;
+}
---
+++ src/family/ParentTest.java
@@ -0,0 +1,12 @@
+package family;
+
+public class ParentTest {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        parent.flatNo = "234";
+        parent.streetName = "SBI";
+        parent.city = "HYD";
+        parent.country = "India";
+        System.out.println(parent.city);
+    }
+}

4f5da88e867f9c58d42d1e18f82fec20be049398
srila <srilakshmiannamdasu@gmail.com>
2024-06-27 16:06:55 +0530

    family1

---
+++ src/family/Child.java
@@ -0,0 +1,14 @@
+package family;
+
+public class Child {
+    String childName;
+    String childAge;
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "childName='" + childName + '\'' +
+                ", childAge='" + childAge + '\'' +
+                '}';
+    }
+}
